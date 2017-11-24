8a80250565c61b212e6006717fe536a1a7cee502
GardenerHan <1129982332@qq.com>
2017-11-24 14:32:01 +0800

    customValidator over

---
+++ customValidator/src/com/hgx/struts2/validation/app/TestValidationAction.java
@@ -0,0 +1,81 @@
+package com.hgx.struts2.validation.app;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestValidationAction extends ActionSupport {
+    private Integer age ;
+    private String password ;
+    private String password2 ;
+    private Integer count ;
+    private  String idCard ;
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword2() {
+        return password2;
+    }
+
+    public void setPassword2(String password2) {
+        this.password2 = password2;
+    }
+
+    @Override
+    public String execute() throws Exception {
+        System.out.println("this:"+this.toString());
+        return  SUCCESS ;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
+    public TestValidationAction() {
+    }
+
+    public TestValidationAction(Integer age, String password, String password2, Integer count, String idCard) {
+        this.age = age;
+        this.password = password;
+        this.password2 = password2;
+        this.count = count;
+        this.idCard = idCard;
+    }
+
+    @Override
+    public String toString() {
+        return "TestValidationAction{" +
+                "age=" + age +
+                ", password='" + password + '\'' +
+                ", password2='" + password2 + '\'' +
+                ", count=" + count +
+                ", idCard=" + idCard +
+                '}';
+    }
+}
+
---
+++ customValidator/web/validation.jsp
@@ -0,0 +1,30 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: guixain
+  Date: 2017/11/21
+  Time: 15:16
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<html>
+<head>
+    <title>validation</title>
+</head>
+<body>
+    <s:debug></s:debug>
+    <br><br>
+    <s:form action="testValidation">
+        <s:textfield name="age" label="age"></s:textfield>
+        <s:password name="password" label="password"></s:password>
+        <s:password name="password2" label="password2"></s:password>
+        <s:textfield name="count" label="count"></s:textfield>
+        <s:textfield name="idCard" label="IDCard"></s:textfield>
+        <s:submit value="提交"></s:submit>
+    </s:form>
+    <s:fielderror></s:fielderror>
+    <s:actionerror></s:actionerror>
+    <%-- map-->数组--%>
+    ${fieldErrors.age[0]}
+</body>
+</html>
